a68cd9729c1c2aab920a4bf1ba8866b2e008c671
mazloomzadeh <86592566+mazloomzadeh@users.noreply.github.com>
2023-12-29 19:45:21 -0500

    Update mysql_cluster_WorderNodeSetup_part1.sh

--- mysql_cluster_WorderNodeSetup_part1.sh
+++ mysql_cluster_WorderNodeSetup_part1.sh
@@ -6,6 +6,16 @@ apt-get -y install sysbench
 # wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 # dpkg -i mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 
+#Install sakila
+mkdir /home/sakila
+cd /home/sakila
+wget https://downloads.mysql.com/docs/sakila-db.tar.gz
+tar -xvzf sakila-db.tar.gz
+#Sakila config
+mysql -Bse "SOURCE /home/sakila/sakila-db/sakila-schema.sql;SOURCE /home/sakila/sakila-db/sakila-data.sql;"
+
+
+# install mysql-cluster
 mkdir -p /opt/mysqlcluster/home
 wget -P /opt/mysqlcluster/home http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 tar xvf /opt/mysqlcluster/home/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
