e155290dcfad890a6e9b0de3421541f4a0998ea9
mazloomzadeh <86592566+mazloomzadeh@users.noreply.github.com>
2023-12-29 19:44:33 -0500

    Update mysql_cluster_MasterNodeSetup_part1.sh

--- mysql_cluster_MasterNodeSetup_part1.sh
+++ mysql_cluster_MasterNodeSetup_part1.sh
@@ -3,6 +3,15 @@ sudo -i
 apt-get update && apt-get -y install libncurses5
 apt-get -y install sysbench
 
+#Install sakila
+mkdir /home/sakila
+cd /home/sakila
+wget https://downloads.mysql.com/docs/sakila-db.tar.gz
+tar -xvzf sakila-db.tar.gz
+#Sakila config
+mysql -Bse "SOURCE /home/sakila/sakila-db/sakila-schema.sql;SOURCE /home/sakila/sakila-db/sakila-data.sql;"
+
+# install mysql-cluster
 mkdir -p /opt/mysqlcluster/home
 wget -P /opt/mysqlcluster/home http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 tar xvf /opt/mysqlcluster/home/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
@@ -25,4 +34,5 @@ datadir=/opt/mysqlcluster/deploy/mysqld_data
 basedir=/opt/mysqlcluster/home/mysqlc
 port=3306
 EOF
+
 
