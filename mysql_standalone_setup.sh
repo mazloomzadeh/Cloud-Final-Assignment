7547a07464b40e9cdcfa2e853716ae0b77836c57
mazloomzadeh <86592566+mazloomzadeh@users.noreply.github.com>
2023-12-29 19:46:13 -0500

    Update mysql_standalone_setup.sh

--- mysql_standalone_setup.sh
+++ mysql_standalone_setup.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/bin/bash -i
 
-# Update package list
-sudo apt-get update -y
-apt-get update && apt-get -y install libncurses5
-apt-get -y install sysbench
+apt-get update -y
+apt-get -y install libncurses5
 
+#Install mysql
+apt-get install mysql-server -y
 
-# Install MySQL Server without interactive prompts
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+#install sakila
+mkdir /home/sakila
+cd /home/sakila
+wget https://downloads.mysql.com/docs/sakila-db.tar.gz
+tar -xvzf sakila-db.tar.gz
+#Sakila config : -B, --batch Don't use history file. Disable interactive behavior. starts with this option ENABLED by default! Disable with; -s, --silent Be more silent. Print results with a tab as separator, Buffer for TCP/IP and socket communication; -e, --execute=name Execute command and quit. (Disables --force and history file)
+mysql -Bse "SOURCE /home/sakila/sakila-db/sakila-schema.sql;SOURCE /home/sakila/sakila-db/sakila-data.sql"
 
-# Start MySQL service
-sudo service mysql start
-
-sudo wget https://downloads.mysql.com/docs/sakila-db.tar.gz
-dpkg -i sakila-db.tar.gz
+#Install sysbench
+apt-get install sysbench -y
 
 
 
